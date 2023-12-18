fd1011282242d692888b85e9be09b2ee855f31e2
npalleta <palleta.sp@gmail.com>
2023-12-17 21:40:49 -0300

    Eureka Service Discovery

---
+++ src/test/java/br/com/npml/discovery/service/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.npml.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
